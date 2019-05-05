000ddac12231cf2c18784a92e557181c61159cb9
Yelyzaveta <yelyzaveta@MacBook-Air-Elizaveta.local>
2019-05-06 00:05:33 +0200

    Updates

---
+++ precache-manifest.66916580b387184597195a9b374aee39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "815f05b264488130ac71",
+    "url": "/react-dictionary-app/static/js/runtime~main.c21074ef.js"
+  },
+  {
+    "revision": "72b691418b066c61fbb8",
+    "url": "/react-dictionary-app/static/js/main.f2bdb79d.chunk.js"
+  },
+  {
+    "revision": "230ea7e0cdd739d4516b",
+    "url": "/react-dictionary-app/static/js/2.0f215bcb.chunk.js"
+  },
+  {
+    "revision": "72b691418b066c61fbb8",
+    "url": "/react-dictionary-app/static/css/main.1089e227.chunk.css"
+  },
+  {
+    "revision": "f41e4fa4908d91726f3724830eff8794",
+    "url": "/react-dictionary-app/index.html"
+  }
+];
